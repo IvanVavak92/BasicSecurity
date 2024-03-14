7246132c77980b956feccf2b74ea42d7ffe9f69c
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-14 13:51:55 +0100

    deleted variables in properties and created users and password encoder in projectSecurityCOnfig

--- BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/config/ProjectSecurityConfig.java
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/config/ProjectSecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +23,27 @@ public class ProjectSecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
                 return http.build();
+    }
+
+    //not recommended
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("12345")) // Encode the password
+                .authorities("admin")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("12345")) // Encode the password
+                .authorities("user")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Use BCryptPasswordEncoder for password encoding
     }
 }
