129ff67dd922009e9ada7e8c38471b045d511b8e
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-14 08:47:16 +0100

    added empty project

---
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/BasicSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.basicsecurityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSecurityProjectApplication.class, args);
+    }
+
+}
---
+++ BasicSecurityProject/src/test/java/com/ivan/basicsecurityproject/BasicSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.basicsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
