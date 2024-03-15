d5d40c6e087daf0469e8be4da7043064785366e3
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-15 12:15:56 +0100

    deleted not used import

--- BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/config/KufrikBankUserDetails.java
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/config/KufrikBankUserDetails.java
@@ -2,7 +2,6 @@ package com.ivan.basicsecurityproject.config;
 
 import com.ivan.basicsecurityproject.models.Customer;
 import com.ivan.basicsecurityproject.repositories.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
