2742513702b01294154623df6775b3a0905b146d
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-15 12:13:50 +0100

    added KufrikBankUSerDetails with loadUSerByUsernam method

---
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/config/KufrikBankUserDetails.java
@@ -0,0 +1,41 @@
+package com.ivan.basicsecurityproject.config;
+
+import com.ivan.basicsecurityproject.models.Customer;
+import com.ivan.basicsecurityproject.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class KufrikBankUserDetails implements UserDetailsService {
+
+
+    private final CustomerRepository customerRepository;
+
+    public KufrikBankUserDetails(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String userName, password = null;
+        List<GrantedAuthority> authorities = null;
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if(customer.size() == 0) {
+            throw new UsernameNotFoundException("User details not found for the User: " + username);
+        } else {
+            userName = customer.get(0).getEmail();
+            password = customer.get(0).getPwd();
+            authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+        }
+        return new User(userName,password,authorities);
+    }
+}
