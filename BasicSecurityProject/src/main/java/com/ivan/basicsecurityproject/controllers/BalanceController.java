32dea38dce8445866e8724483025b04c5c83aaab
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-16 12:08:50 +0100

    changed method in balanceController

--- BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/controllers/BalanceController.java
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/controllers/BalanceController.java
@@ -1,13 +1,30 @@
 package com.ivan.basicsecurityproject.controllers;
 
+import com.ivan.basicsecurityproject.models.AccountTransactions;
+import com.ivan.basicsecurityproject.repositories.AccountTransactionsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BalanceController {
 
+    private final AccountTransactionsRepository accountTransactionsRepository;
+
+    public BalanceController(AccountTransactionsRepository accountTransactionsRepository) {
+        this.accountTransactionsRepository = accountTransactionsRepository;
+    }
+
     @GetMapping("/myBalance")
-    public String getBalanceDetails() {
-        return "Here are balance details from DB";
+    public List<AccountTransactions> getBalanceDetails(@RequestParam int id) {
+        List<AccountTransactions> accountTransactions = accountTransactionsRepository.
+                findByCustomerIdOrderByTransactionDtDesc(id);
+        if (accountTransactions != null) {
+            return accountTransactions;
+        } else {
+            return null;
+        }
     }
 }
