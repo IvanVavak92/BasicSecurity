8b6a0776050aa402f3276a3d17c4c4627e83080b
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-16 12:03:54 +0100

    changed getAccountDetails method in account controller

--- BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/controllers/AccountController.java
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/controllers/AccountController.java
@@ -1,13 +1,27 @@
 package com.ivan.basicsecurityproject.controllers;
 
+import com.ivan.basicsecurityproject.models.Accounts;
+import com.ivan.basicsecurityproject.repositories.AccountsRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
 
+    private final AccountsRepository accountsRepository;
+
+    public AccountController(AccountsRepository accountsRepository) {
+        this.accountsRepository = accountsRepository;
+    }
+
     @GetMapping("/myAccount")
-    public String getAccountDetails() {
-        return "Here are the account details from DB.";
+    public Accounts getAccountDetails(@RequestParam int id) {
+        Accounts accounts = accountsRepository.findByCustomerId(id);
+        if (accounts != null) {
+            return accounts;
+        } else {
+            return null;
+        }
     }
 }
