c4522885e34e8053e579461d432fea59a7a4737c
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-16 12:34:21 +0100

    added new method in LoginCOntroller

--- BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/controllers/LoginController.java
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/controllers/LoginController.java
@@ -5,10 +5,14 @@ import com.ivan.basicsecurityproject.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoginController {
@@ -42,5 +46,16 @@ public class LoginController {
                     .body("Exception occured dure to " + e.getMessage());
         }
         return response;
+    }
+
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        List<Customer> customers = customerRepository.findByEmail(authentication.getName());
+        if (customers.size() > 0) {
+            return customers.get(0);
+        } else {
+            return null;
+        }
+
     }
 }
