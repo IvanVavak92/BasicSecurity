b014df4036c74462ed7071aa578ea16da335c6dd
Ivan Vavak <ivanvavak92@gmail.com>
2024-03-16 11:34:38 +0100

    added new models

---
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/models/Contact.java
@@ -0,0 +1,74 @@
+package com.ivan.basicsecurityproject.models;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String contactId;
+
+    private String contactName;
+
+
+    private String contactEmail;
+
+    private String subject;
+
+    private String message;
+
+    private Date createDt;
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(Date createDt) {
+        this.createDt = createDt;
+    }
+
+}
+
---
+++ BasicSecurityProject/src/main/java/com/ivan/basicsecurityproject/models/Notice.java
@@ -0,0 +1,78 @@
+package com.ivan.basicsecurityproject.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.sql.Date;
+
+@Entity
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int noticeId;
+
+    private String noticeSummary;
+    private String noticeDetails;
+    private Date noticBegDt;
+    private Date noticEndDt;
+    private Date createDt;
+
+    private Date updateDt;
+
+    public int getNoticeId() {
+        return noticeId;
+    }
+
+    public void setNoticeId(int noticeId) {
+        this.noticeId = noticeId;
+    }
+
+    public String getNoticeSummary() {
+        return noticeSummary;
+    }
+
+    public void setNoticeSummary(String noticeSummary) {
+        this.noticeSummary = noticeSummary;
+    }
+
+    public String getNoticeDetails() {
+        return noticeDetails;
+    }
+
+    public void setNoticeDetails(String noticeDetails) {
+        this.noticeDetails = noticeDetails;
+    }
+
+    public Date getNoticBegDt() {
+        return noticBegDt;
+    }
+
+    public void setNoticBegDt(Date noticBegDt) {
+        this.noticBegDt = noticBegDt;
+    }
+
+    public Date getNoticEndDt() {
+        return noticEndDt;
+    }
+
+    public void setNoticEndDt(Date noticEndDt) {
+        this.noticEndDt = noticEndDt;
+    }
+
+    public Date getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(Date createDt) {
+        this.createDt = createDt;
+    }
+
+    public Date getUpdateDt() {
+        return updateDt;
+    }
+
+    public void setUpdateDt(Date updateDt) {
+        this.updateDt = updateDt;
+    }
+}
